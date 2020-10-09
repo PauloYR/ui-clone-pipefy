4e32f579f5fd2349b6cd3e8831db8aa4ac88352c
Paulo Iury <pauloyr@outlook.com>
2020-10-09 17:00:17 -0300

    melhorando estilo do scroll das listas

--- src/components/List/styles.ts
+++ src/components/List/styles.ts
@@ -38,7 +38,7 @@ export const Container = styled.div< ContainerData >`
     ul{
         margin-top: 30px;
         width: 100%;
-        height: 100%;        
+        height: 90%;        
         overflow-y: auto;  
     }
   
